98048c54a62cd5e890c188dc18a927c876106476
Yuval Kogman <nothingmuch@woobling.org>
2008-12-26 22:07:55 +0200

    doc update

--- lib/Task/KiokuDB.pm
+++ lib/Task/KiokuDB.pm
@@ -25,19 +25,35 @@ Task::KiokuDB - Install L<KiokuDB> and related modules.
 
 =head1 DESCRIPTION
 
+This bundle installs L<KiokuDB> and depending on the environment several
+additional components.
+
 =head1 MODULES
+
+All of these are considered optional.
 
 =over 4
 
-=item L<KiokuDB::Backend::JSPON>
+=item L<KiokuDB::Backend::Files>
 
-The JSON file backend.
+The file backend. Defaults to true.
 
 =item L<KiokuDB::Backend::BDB>
 
-L<BerkeleyDB> backend. Requires an installed BerkeleyDB, preferably 4.7.
+L<BerkeleyDB> backend. Requires an installed BerkeleyDB, preferably linked
+against 4.7.
 
-Defaults to false.
+Defaults false to unless BDB seems to be installed.
+
+=item L<KiokuDB::Backend::DBI>
+
+The L<DBI> backend.
+
+Defaults to true if you have one of the tested DBDs (SQLite, Pg, or mysql)
+
+=item L<KiokuDB::Backend::CouchDB>
+
+Defaults to true if you have L<AnyEvent::CouchDB>.
 
 =item L<Data::UUID::LibUUID>
 
@@ -45,7 +61,20 @@ Better UUID generation than L<Data::UUID> (not time based). Requires C<libuuid>
 which is available by default on Mac OS X, and easy to install on most Linux
 distributions.
 
-Defaults to false.
+Defaults to false, unless C<uuid.h> can be found and you have a compiler.
+
+=item L<JSON::XS>
+
+Provides faster JSON performance.
+
+Defaults to true if you have a compiler.
+
+=item L<YAML::XS>
+
+Allows dumping/loading DBs as YAML, using YAML serialization, and importing to
+a DB using L<MooseX::YAML> with modern/up to spec YAML files.
+
+Defaults to true if you have a compiler.
 
 =back
 
